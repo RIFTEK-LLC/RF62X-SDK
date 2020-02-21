1fe742965280de8538aa900c276ecf93c6e7c15f
Владислав Кузьмин <v.kuzmin@a.riftek.com>
2020-02-21 14:31:29 +0300

    rf627sdk v.1.2.0

--- samples/win64/CMake/RF627_params/main.cpp
+++ samples/win64/CMake/RF627_params/main.cpp
@@ -26,21 +26,52 @@ int main()
     std::cout << "Discovered " << list.size() << " rf627-old" << std::endl;
 
     /*
-     * Iterate over all discovered rf627-old in network, connect to each of
-     * them and get a profile.
-     */
-    for(size_t i = 0; i < list.size(); i++)
-    {
-        // Establish connection to the RF627 device by Service Protocol.
-        list[i]->connect();
-        // Get profile from scanner's data stream by Service Protocol.
-        profile_t* profile = list[i]->get_profile();
-        if (profile != nullptr)
-        {
-            // Print the serial number of the scanner to which the profile belongs.
-            std::cout << profile->profile_header.serial_number << std::endl;
-        }
-    }
+        * Iterate over all discovered rf627-old in network, connect to each of
+        * them and get a profile.
+        */
+       for(size_t i = 0; i < list.size(); i++)
+       {
+           // Establish connection to the RF627 device by Service Protocol.
+           list[i]->connect();
+
+           // read params from RF627 device by Service Protocol.
+           list[i]->read_params();
+           param_t* a = NULL;
+
+           for(size_t j = 0; j < sizeof(parameter_names)/sizeof(parameter_names[0]); j++)
+           {
+               // Get parameter by it's name from last read
+               a = list[i]->get_param(parameter_names[j]);
+               std::cout << parameter_names[j] << " ";
+               if(a != nullptr)
+               {
+                   if (a->type == pvtKey[PVT_STRING])
+                       std::cout << a->get_value<value_str_t>() << std::endl;
+                   else
+                       if (a->type == pvtKey[PVT_INT])
+                           std::cout << a->get_value<value_int32_t>() << std::endl;
+                       else
+                           if (a->type == pvtKey[PVT_INT64])
+                               std::cout << a->get_value<value_int64_t>() << std::endl;
+                           else
+                               if (a->type == pvtKey[PVT_UINT])
+                                   std::cout << a->get_value<value_uint32_t>() << std::endl;
+                               else
+                                   if (a->type == pvtKey[PVT_UINT64])
+                                       std::cout << a->get_value<value_uint64_t>() << std::endl;
+                                   else
+                                       if (a->type == pvtKey[PVT_FLOAT])
+                                           std::cout << a->get_value<value_flt_t>() << std::endl;
+                                       else
+                                           if (a->type == pvtKey[PVT_DOUBLE])
+                                               std::cout << a->get_value<value_dbl_t>() << std::endl;
+               }else
+               {
+                   std::cout << "null" << std::endl;
+               }
+           }
+       }
+
 }
 
 
