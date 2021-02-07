529855a64794a3b2fd2e43070f1bb2094ebf5f72
Владислав Кузьмин <v.kuzmin@a.riftek.com>
2021-02-07 14:32:27 +0300

    updated examples for C wrapper

---
+++ examples/C/RF62X_smart/RF62X_profile/src/main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "network.h"
+#include "rf62Xcore.h"
+#include "rf62X_sdk.h"
+#include "rf62X_types.h"
+
+int main()
+{
+
+    // Initialize sdk library
+    core_init();
+
+    // Print return rf627 sdk version
+    printf("SDK version: %s\n", sdk_version());
+    printf("=========================================\n");
+
+
+    // Create value for scanners vector's type
+    vector_t* scanners = (vector_t*)calloc(1, sizeof (vector_t));
+    // Initialization vector
+    vector_init(&scanners);
+
+    // Iterate over all available network adapters in the current operating
+    // system to send "Hello" requests.
+    for (int i=0; i<GetAdaptersCount(); i++)
+    {
+        // Get another IP Addr and set this changes in adapter settings.
+        uint32_t host_ip_addr = ntohl(inet_addr(GetAdapterAddress(i)));
+        uint32_t host_mask = ntohl(inet_addr("255.255.255.0"));
+        // call the function to change adapter settings inside the library.
+        set_platform_adapter_settings(host_mask, host_ip_addr);
+
+        // Search for RF627-old devices over network by Service Protocol.
+        search_scanners(scanners, kRF627_SMART, 500, kSERVICE);
+    }
+
+    // Print count of discovered rf627-smart in network by Service Protocol
+    printf("Discovered: %d rf627-smart\n", (int)vector_count(scanners));
+
+    for (int i = 0; i < (int)vector_count(scanners); i++)
+    {
+        scanner_base_t* scanner = vector_get(scanners,i);
+
+        hello_information result = get_info_about_scanner(scanner, kSERVICE);
+
+        rf627_smart_hello_info_by_service_protocol* info =
+                result.rf627smart.hello_info_service_protocol;
+
+        printf("\n\n\nID scanner's list: %d\n", i);
+        printf("-----------------------------------------\n");
+        printf("Device information: \n");
+        printf("* Name\t\t: %s\n", info->user_general_deviceName);
+        printf("* Serial\t: %d\n", info->fact_general_serial);
+        printf("* IP Addr\t: %s\n", info->user_network_ip);
+        printf("* MAC Addr\t: %s\n", info->fact_network_macAddr);
+
+        uint8_t is_connected = connect_to_scanner(scanner, kSERVICE);
+
+        if (is_connected)
+        {
+            uint8_t zero_points = TRUE;
+            // Get profile from scanner's data stream by Service Protocol.
+            rf627_profile2D_t* result = get_profile2D_from_scanner(
+                        scanner, zero_points, kSERVICE);
+            rf627_smart_profile2D_t* profile2D = result->rf627smart_profile2D;
+            if (profile2D != NULL)
+            {
+                printf("Profile information: \n");
+                switch (profile2D->header.data_type)
+                {
+                case (int)DTY_PixelsNormal:
+                {
+                    printf("* DataType\t: PIXELS\n");
+                    printf("* Count\t\t: %d\n", profile2D->pixels_format.pixels_count);
+                    break;
+                }
+                case (int)DTY_PixelsInterpolated:
+                {
+                    printf("* DataType\t: PIXELS_INTRP\n");
+                    printf("* Count\t\t: %d\n", profile2D->pixels_format.pixels_count);
+                    break;
+                }
+                case (int)DTY_ProfileNormal:
+                {
+                    printf("* DataType\t: PROFILE\n");
+                    printf("* Count\t\t: %d\n", profile2D->profile_format.points_count);
+                    break;
+                }
+                case (int)DTY_ProfileInterpolated:
+                {
+                    printf("* DataType\t: PROFILE_INTRP\n");
+                    printf("* Count\t\t: %d\n", profile2D->profile_format.points_count);
+                    break;
+                }
+                }
+                printf("Profile was successfully received!\n");
+                printf("-----------------------------------------\n");
+                free(profile2D);
+            }else
+            {
+                printf("Profile was not received!\n");
+                printf("-----------------------------------------\n");
+            }
+        }
+
+        disconnect_from_scanner(scanner, kSERVICE);
+    }
+
+
+    // Cleanup resources allocated with core_init()
+    FreeAdapterAddresses();
+    WinSockDeinit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
