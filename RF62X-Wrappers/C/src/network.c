a548b74f4d685681c4ce62427fe69e7c7501f896
KuzVlad <kuzvlad1993@gmail.com>
2021-05-13 01:03:05 +0300

    updated network for linux

--- RF62X-Wrappers/C/src/network.c
+++ RF62X-Wrappers/C/src/network.c
@@ -140,78 +140,77 @@ BOOL EnumAdapterAddresses()
     if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == NO_ERROR) {
         pAdapter = pAdapterInfo;
         while (pAdapter) {
-            printf("\tComboIndex: \t%d\n", pAdapter->ComboIndex);
-            printf("\tAdapter Name: \t%s\n", pAdapter->AdapterName);
-            printf("\tAdapter Desc: \t%s\n", pAdapter->Description);
-            printf("\tAdapter Addr: \t");
-            for (i = 0; i < pAdapter->AddressLength; i++) {
-                if (i == (pAdapter->AddressLength - 1))
-                    printf("%.2X\n", (int) pAdapter->Address[i]);
-                else
-                    printf("%.2X-", (int) pAdapter->Address[i]);
-            }
-            printf("\tIndex: \t%d\n", pAdapter->Index);
-            printf("\tType: \t");
-            switch (pAdapter->Type) {
-            case MIB_IF_TYPE_OTHER:
-                printf("Other\n");
-                break;
-            case MIB_IF_TYPE_ETHERNET:
-                printf("Ethernet\n");
-                break;
-            case MIB_IF_TYPE_TOKENRING:
-                printf("Token Ring\n");
-                break;
-            case MIB_IF_TYPE_FDDI:
-                printf("FDDI\n");
-                break;
-            case MIB_IF_TYPE_PPP:
-                printf("PPP\n");
-                break;
-            case MIB_IF_TYPE_LOOPBACK:
-                printf("Lookback\n");
-                break;
-            case MIB_IF_TYPE_SLIP:
-                printf("Slip\n");
-                break;
-            default:
-                printf("Unknown type %ld\n", pAdapter->Type);
-                break;
-            }
+//            printf("\tComboIndex: \t%d\n", pAdapter->ComboIndex);
+//            printf("\tAdapter Name: \t%s\n", pAdapter->AdapterName);
+//            printf("\tAdapter Desc: \t%s\n", pAdapter->Description);
+//            printf("\tAdapter Addr: \t");
+//            for (i = 0; i < pAdapter->AddressLength; i++) {
+//                if (i == (pAdapter->AddressLength - 1))
+//                    printf("%.2X\n", (int) pAdapter->Address[i]);
+//                else
+//                    printf("%.2X-", (int) pAdapter->Address[i]);
+//            }
+//            printf("\tIndex: \t%d\n", pAdapter->Index);
+//            printf("\tType: \t");
+//            switch (pAdapter->Type) {
+//            case MIB_IF_TYPE_OTHER:
+//                printf("Other\n");
+//                break;
+//            case MIB_IF_TYPE_ETHERNET:
+//                printf("Ethernet\n");
+//                break;
+//            case MIB_IF_TYPE_TOKENRING:
+//                printf("Token Ring\n");
+//                break;
+//            case MIB_IF_TYPE_FDDI:
+//                printf("FDDI\n");
+//                break;
+//            case MIB_IF_TYPE_PPP:
+//                printf("PPP\n");
+//                break;
+//            case MIB_IF_TYPE_LOOPBACK:
+//                printf("Lookback\n");
+//                break;
+//            case MIB_IF_TYPE_SLIP:
+//                printf("Slip\n");
+//                break;
+//            default:
+//                printf("Unknown type %ld\n", pAdapter->Type);
+//                break;
+//            }
 
             _ifs[_ifs_cnt] = (char*)malloc(64);
             memset(_ifs[_ifs_cnt], 0, 64);
             sprintf(_ifs[_ifs_cnt++], "%s",pAdapter->IpAddressList.IpAddress.String);
-            printf("\tIP Address: \t%s\n", pAdapter->IpAddressList.IpAddress.String);
+//            printf("\tIP Address: \t%s\n", pAdapter->IpAddressList.IpAddress.String);
             _mfs[_mfs_cnt] = (char*)malloc(64);
             memset(_mfs[_mfs_cnt], 0, 64);
             sprintf(_mfs[_mfs_cnt++], "%s",pAdapter->IpAddressList.IpMask.String);
-            printf("\tIP Mask: \t%s\n", pAdapter->IpAddressList.IpMask.String);
+//            printf("\tIP Mask: \t%s\n", pAdapter->IpAddressList.IpMask.String);
 
-            printf("\tGateway: \t%s\n", pAdapter->GatewayList.IpAddress.String);
-            printf("\t***\n");
+//            printf("\tGateway: \t%s\n", pAdapter->GatewayList.IpAddress.String);
+//            printf("\t***\n");
 
-            if (pAdapter->DhcpEnabled) {
-                printf("\tDHCP Enabled: Yes\n");
-                printf("\t  DHCP Server: \t%s\n",
-                       pAdapter->DhcpServer.IpAddress.String);
-            } else
-                printf("\tDHCP Enabled: No\n");
+//            if (pAdapter->DhcpEnabled) {
+//                printf("\tDHCP Enabled: Yes\n");
+//                printf("\t  DHCP Server: \t%s\n",
+//                       pAdapter->DhcpServer.IpAddress.String);
+//            } else
+//                printf("\tDHCP Enabled: No\n");
 
-            if (pAdapter->HaveWins) {
-                printf("\tHave Wins: Yes\n");
-                printf("\t  Primary Wins Server:    %s\n",
-                       pAdapter->PrimaryWinsServer.IpAddress.String);
-                printf("\t  Secondary Wins Server:  %s\n",
-                       pAdapter->SecondaryWinsServer.IpAddress.String);
-            } else
-                printf("\tHave Wins: No\n");
+//            if (pAdapter->HaveWins) {
+//                printf("\tHave Wins: Yes\n");
+//                printf("\t  Primary Wins Server:    %s\n",
+//                       pAdapter->PrimaryWinsServer.IpAddress.String);
+//                printf("\t  Secondary Wins Server:  %s\n",
+//                       pAdapter->SecondaryWinsServer.IpAddress.String);
+//            } else
+//                printf("\tHave Wins: No\n");
             pAdapter = pAdapter->Next;
-            printf("\n");
+//            printf("\n");
         }
     } else {
         printf("GetAdaptersInfo failed with error: %d\n", dwRetVal);
-
     }
 
     if (pAdapterInfo)
@@ -233,7 +232,19 @@ BOOL EnumAdapterAddresses()
         {
             _ifs[_ifs_cnt] = (char*)malloc(64);
             memset(_ifs[_ifs_cnt], 0, 64);
-            sprintf(_ifs[_ifs_cnt++], "%u.%u.%u.%u", (BYTE)(tmp->ifa_addr->sa_data[2]), (BYTE)(tmp->ifa_addr->sa_data[3]), (BYTE)(tmp->ifa_addr->sa_data[4]), (BYTE)(tmp->ifa_addr->sa_data[5]));
+            sprintf(_ifs[_ifs_cnt++], "%u.%u.%u.%u",
+                    (BYTE)(tmp->ifa_addr->sa_data[2]),
+                    (BYTE)(tmp->ifa_addr->sa_data[3]),
+                    (BYTE)(tmp->ifa_addr->sa_data[4]),
+                    (BYTE)(tmp->ifa_addr->sa_data[5]));
+
+            _mfs[_mfs_cnt] = (char*)malloc(64);
+            memset(_mfs[_mfs_cnt], 0, 64);
+            sprintf(_mfs[_mfs_cnt++], "%u.%u.%u.%u",
+                    (BYTE)(tmp->ifa_netmask->sa_data[2]),
+                    (BYTE)(tmp->ifa_netmask->sa_data[3]),
+                    (BYTE)(tmp->ifa_netmask->sa_data[4]),
+                    (BYTE)(tmp->ifa_netmask->sa_data[5]));
         }
         tmp = tmp->ifa_next;
     }
@@ -252,6 +263,14 @@ void FreeAdapterAddresses()
         }
     }
     _ifs_cnt = 0;
+
+    for(int i=0; i<_mfs_cnt; i++) {
+        if (_mfs[i]) {
+            free(_mfs[i]);
+            _mfs[i] = NULL;
+        }
+    }
+    _mfs_cnt = 0;
 }
 
 BOOL MatchIP(const char* ip1, const char* ip2, const char* mask)
