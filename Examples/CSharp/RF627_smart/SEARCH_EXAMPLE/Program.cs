9c700b60184c184a5e7dc8acd9a3950f501de1a9
KuzVlad <kuzvlad1993@gmail.com>
2021-05-27 03:35:19 +0300

    added Examples/CSharp/RF627_smart/SEARCH_EXAMPLE

---
+++ Examples/CSharp/RF627_smart/SEARCH_EXAMPLE/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using SDK.SCANNERS;
+
+namespace SEARCH_EXAMPLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#########################################");
+            Console.WriteLine("#                                       #");
+            Console.WriteLine("#         Search Example v2.x.x         #");
+            Console.WriteLine("#                                       #");
+            Console.WriteLine("#########################################");
+
+            // Initialize sdk library
+            RF62X.SdkInit();
+
+            // Print return rf62X sdk version
+            Console.WriteLine("SDK version: {0}", RF62X.SdkVersion());
+            Console.WriteLine("=========================================");
+
+            // Search for RF627smart devices over network
+            List<RF62X.RF627smart> list = RF62X.RF627smart.Search(500);
+
+            // Print count of discovered rf627smart in network by Service Protocol
+            Console.WriteLine("Was found\t: {0} RF627-Smart", list.Count);
+            Console.WriteLine("=========================================");
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                RF62X.HelloInfo info = list[i].GetInfo();
+
+                Console.WriteLine("\n\nID scanner's list: {0}", i);
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Device information: ");
+                Console.WriteLine("* Name\t: {0}", info.device_name);
+                Console.WriteLine("* Serial\t: {0}", info.serial_number);
+                Console.WriteLine("* IP Addr\t: {0}", info.ip_address);
+                Console.WriteLine("* MAC Addr\t: {0}", info.mac_address);
+
+                Console.WriteLine("\nWorking ranges: ");
+                Console.WriteLine("* Zsmr, mm\t: {0}", info.z_smr);
+                Console.WriteLine("* Zmr , mm\t: {0}", info.z_mr);
+                Console.WriteLine("* Xsmr, mm\t: {0}", info.x_smr);
+                Console.WriteLine("* Xemr, mm\t: {0}", info.x_emr);
+
+                Console.WriteLine("\nVersions: ");
+                Console.WriteLine("* Firmware\t: {0}", info.firmware_version);
+                Console.WriteLine("* Hardware\t: {0}", info.hardware_version);
+                Console.WriteLine("-----------------------------------------");
+            }
+
+            Console.WriteLine("{0}Press any key to end \"Search Example\"", Environment.NewLine);
+            Console.ReadKey();
+
+            // Cleanup resources allocated with SdkInit()
+            RF62X.SdkCleanup();
+        }
+    }
+}
