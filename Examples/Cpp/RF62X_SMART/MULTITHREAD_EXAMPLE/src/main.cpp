902639b5d5d9448b807172fd8774cc258161f431
KuzVlad <kuzvlad1993@gmail.com>
2022-01-31 12:45:31 +0300

    updated MULTITHREAD_EXAMPLE for c++

--- Examples/Cpp/RF62X_SMART/MULTITHREAD_EXAMPLE/src/main.cpp
+++ Examples/Cpp/RF62X_SMART/MULTITHREAD_EXAMPLE/src/main.cpp
@@ -8,20 +8,8 @@
 
 using namespace SDK::SCANNERS::RF62X;
 
-
-
-/**
- * @brief Function to get profile
- * @param obj Ptr to std::shared_ptr<rf627smart>
- */
+void read_params_func(std::shared_ptr<rf627smart>& obj);
 void get_profile_func(std::shared_ptr<rf627smart>& obj);
-
-/**
- * @brief Function to get/set parameters
- * @param obj Ptr to std::shared_ptr<rf627smart>
- */
-void laser_on_func(std::shared_ptr<rf627smart>& obj);
-void laser_off_func(std::shared_ptr<rf627smart>& obj);
 
 int main()
 {
@@ -51,18 +39,32 @@ int main()
 
     std::vector<std::thread*> thread_list;
 
-    std::cout << "\nTEST 1: Get profiles in different threads" << std::endl;
+    std::cout << "\nTEST 1: Read params in different threads" << std::endl;
     std::cout << "========================================="     << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     // Iterate over all discovered scanners in network, connect to each of them,
-    // get profiles in different threads.
+    // read params in different threads.
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        std::thread* profile_thread1 = new std::thread(read_params_func, ref(list[i]));
+        thread_list.push_back(profile_thread1);
+    }
+
+    for (size_t i = 0; i < thread_list.size(); i++)
+    {
+        if (thread_list[i]->joinable())
+            thread_list[i]->join();
+    }
+    thread_list.clear();
+
+    std::cout << "\nTEST 2: Get profiles in different threads" << std::endl;
+    std::cout << "========================================="     << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
     for (size_t i = 0; i < list.size(); i++)
     {
         std::thread* profile_thread1 = new std::thread(get_profile_func, ref(list[i]));
-        std::thread* profile_thread2 = new std::thread(get_profile_func, ref(list[i]));
-
         thread_list.push_back(profile_thread1);
-        thread_list.push_back(profile_thread2);
     }
 
     for (size_t i = 0; i < thread_list.size(); i++)
@@ -70,25 +72,7 @@ int main()
         if (thread_list[i]->joinable())
             thread_list[i]->join();
     }
-
-    std::cout << "\nTEST 2: Get/Set parameters in different threads" << std::endl;
-    std::cout << "========================================="     << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    // Get the Laser Enabled parameter and turn it on in one thread, turn it off in the other
-    for (size_t i = 0; i < list.size(); i++)
-    {
-        std::thread* param_thread1 = new std::thread(laser_on_func, ref(list[i]));
-        std::thread* param_thread2 = new std::thread(laser_off_func, ref(list[i]));
-
-        thread_list.push_back(param_thread1);
-        thread_list.push_back(param_thread2);
-    }
-
-    for (size_t i = 0; i < thread_list.size(); i++)
-    {
-        if (thread_list[i]->joinable())
-            thread_list[i]->join();
-    }
+    thread_list.clear();
 
     for (size_t i = 0; i < list.size(); i++)
     {
@@ -101,29 +85,6 @@ int main()
 }
 
 std::mutex print_mutex;
-void print_profile_info(std::shared_ptr<profile2D>& profile,
-                        std::thread::id thread_id, int& count);
-
-void get_profile_func(std::shared_ptr<rf627smart>& scanner)
-{
-    // Establish connection to the RF627 device by Service Protocol.
-    bool is_connected = scanner->connect();
-
-    // Get profile from scanner's data stream by Service Protocol.
-    std::shared_ptr<profile2D> profile = nullptr;
-    bool zero_points = true;
-    bool realtime = true;
-
-    int count = 0;
-    while(count < 20)
-    if (is_connected)
-    {
-        profile = scanner->get_profile2D(zero_points,realtime);
-        print_profile_info(profile, std::this_thread::get_id(), count);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-}
-
 void print_profile_info(std::shared_ptr<profile2D>& profile,
                         std::thread::id thread_id, int& count)
 {
@@ -161,93 +122,54 @@ void print_profile_info(std::shared_ptr<profile2D>& profile,
     print_mutex.unlock();
 }
 
-void print_laser_info(bool is_enable, std::thread::id thread_id, int& count);
-void laser_on_func(std::shared_ptr<rf627smart>& obj)
+void get_profile_func(std::shared_ptr<rf627smart>& scanner)
 {
     // Establish connection to the RF627 device by Service Protocol.
-    bool is_connected = obj->connect();
+    bool is_connected = scanner->connect();
+
+    // Get profile from scanner's data stream by Service Protocol.
+    std::shared_ptr<profile2D> profile = nullptr;
+    bool zero_points = true;
+    bool realtime = true;
 
     int count = 0;
     while(count < 20)
+    if (is_connected)
     {
-        // read params from RF627 device by Service Protocol.
-        bool is_read = obj->read_params();
-
-        if (is_connected && is_read)
-        {
-            // Get parameter of Laser Enabled
-            std::shared_ptr<param> laser_enabled = obj->get_param("user_laser_enabled");
-            if (laser_enabled != nullptr)
-            {
-                bool isEnabled = laser_enabled->getValue<uint32_t>();
-                if (isEnabled == false)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                    laser_enabled->setValue<uint32_t>(true);
-                    obj->set_param(laser_enabled);
-                    obj->write_params();
-                    print_laser_info(true, std::this_thread::get_id(), count);
-                }else
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                    count++;
-                }
-            }
-        }
+        profile = scanner->get_profile2D(zero_points,realtime);
+        print_profile_info(profile, std::this_thread::get_id(), count);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
-void laser_off_func(std::shared_ptr<rf627smart>& obj)
-{
-    // Establish connection to the RF627 device by Service Protocol.
-    bool is_connected = obj->connect();
-
-
-    int count = 0;
-    while(count < 10)
-    {
-        // read params from RF627 device by Service Protocol.
-        bool is_read = obj->read_params();
-
-        if (is_connected && is_read)
-        {
-            // Get parameter of Laser Enabled
-            std::shared_ptr<param> laser_enabled = obj->get_param("user_laser_enabled");
-            if (laser_enabled != nullptr)
-            {
-                bool isEnabled = laser_enabled->getValue<uint32_t>();
-                if (isEnabled == true)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                    laser_enabled->setValue<uint32_t>(false);
-                    obj->set_param(laser_enabled);
-                    obj->write_params();
-                    print_laser_info(false, std::this_thread::get_id(), count);
-                }else
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                    count++;
-                }
-            }
-        }
-    }
-}
-
-void print_laser_info(bool is_enable, std::thread::id thread_id, int& count)
+void print_read_params_info(bool is_read, std::thread::id thread_id, int& count)
 {
     print_mutex.lock();
-    if (is_enable)
+    if (is_read)
     {
-        std::cout << "Laser switched OFF from thread_id = " << thread_id <<std::endl;
+        std::cout << "Params read from thread_id = " << thread_id <<std::endl;
         count++;
     }else
     {
-        std::cout << "Laser switched ON from thread_id = " << thread_id <<std::endl;
+        std::cout << "Params not read from thread_id = " << thread_id <<std::endl;
         count++;
     }
     print_mutex.unlock();
 }
 
+void read_params_func(std::shared_ptr<rf627smart>& obj)
+{
+    // Establish connection to the RF627 device by Service Protocol.
+    bool is_connected = obj->connect();
+
+    int count = 0;
+    if (is_connected)
+    while(count < 10)
+    {
+        // read params from RF627 device by Service Protocol.
+        print_read_params_info(obj->read_params(), std::this_thread::get_id(), count);
+    }
+}
 
 
 
